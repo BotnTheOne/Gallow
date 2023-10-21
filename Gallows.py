63a8638371c7967e8710fa1f1a88ec962187babe
Botn <botn@bk.ru>
2023-10-21 13:42:39 +0400

    First commit

---
+++ Gallows.py
@@ -0,0 +1,58 @@
+# РАБОТАЙ через создание функций def, разделяя каждое действие!!!!
+# Консольная игра "Виселица"
+
+
+import random
+
+
+def nouns():
+    """Логика окрытия словаря"""
+    with open('russian_nouns.txt') as nouns:
+        text = [line.strip() for line in nouns]
+    return text
+
+
+def prog_start():
+    """Логика старта программы"""
+    start = input('Do you want to play? (press y or n) ')
+    for i in start:
+        if i == 'y':
+            print('PC choose a random word. If you guess it, you won!')
+        else:
+            print('Exit')
+            exit()
+    return start
+
+
+def random_word():
+    """Логика выбора случайного слова"""
+    word = random.choice(nouns())
+    # print(word)
+    return word
+
+
+def human_letter():
+    """Логика ввода пользователем буквы"""
+    insert = input('Enter one letter: ')
+    return insert
+
+
+def is_part_in_word(human_letter, random_word):
+    """Логика сравнения введенной буквы и загаданного слова"""
+    for w in random_word:
+        if w.lower() in human_letter.lower():
+            return True
+    return False
+
+
+def picture():
+    """Логика отрисовки виселицы"""
+    pass
+
+
+prog_start()
+random_word()
+print(random_word())
+# human_word()
+print(human_letter())
+print(is_part_in_word(human_letter(), random_word()))
